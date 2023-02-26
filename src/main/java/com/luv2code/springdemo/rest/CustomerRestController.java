8d2124e40e1593b98815fe6fa1ccf39f2dc85d97
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2023-02-26 17:00:08 +0100

    feat: add rest services get customer and add customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,10 +3,7 @@ package com.luv2code.springdemo.rest;
 import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,19 @@ public class CustomerRestController {
             throw new CustomerNotFoundException("Customer id not found - " + customerId);
         }
         return theCustomer;
+    }
+
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer theCustomer) {
+
+        theCustomer.setId(0); //hibernate method saveOrUpdate() need id == null to save new item instead of update
+
+        customerService.saveCustomer(theCustomer);
+        return theCustomer;
+    }
+
+    @DeleteMapping("/customers/{customerId}")
+    public void deleteCustomer(@PathVariable int customerId) {
+        customerService.deleteCustomer(customerId);
     }
 }
